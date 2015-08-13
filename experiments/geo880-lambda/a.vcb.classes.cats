6da71292a2b72fc6c6f2ee164d15501bbdb4e74e
fereshte <fereshtekhani@gmail.com>
2015-08-13 16:06:44 -0700

    adding data

---
+++ experiments/geo880-lambda/a.vcb.classes.cats
@@ -0,0 +1,59 @@
+;KategProblem:cats: 50   words: 50
+0:$,
+1:
+2:en.location,en.meeting,en.person,
+3:<=,
+4:
+5:aggregate,
+6:listValue,
+7:type,
+8:<,>,
+9:sum,
+10:1,
+11:avg,
+12:2,
+13:en.person.bob,
+14:en.meeting.annual_review,
+15:string,
+16:getProperty,
+17:filter,
+18:singleton,
+19:length,
+20:min,
+21:number,
+22:en.person.alice,
+23:location,
+24:2015,
+25:3,
+26:en.hour,
+27:concat,
+28:countComparative,
+29:10,15,
+30:
+31:reverse,
+32:attendee,
+33:ensureNumericProperty,
+34:time,
+35:superlative,
+36:end_time,
+37:countSuperlative,
+38:>=,
+39:!=,
+40:is_important,
+41:max,
+42:=,
+43:0,
+44:ensureNumericEntity,
+45:
+46:en.meeting.weekly_standup,
+47:en.location.greenberg_cafe,
+48:date,
+49:start_time,
+50:
+51:
+;I have 46 categories used.
+
+Problem(1,0,208)
+      #value: 6
+#valueChange: 50089
+   #doChange: 50004
---
+++ experiments/geo880-lambda/change2.sh
@@ -0,0 +1,71 @@
+#! /bin/csh
+
+echo `rm run.test`;
+
+set p = 1;
+while ($p <= 1)
+echo "portion is $p";
+
+set classpath="../../src:.";
+
+
+echo `javac -classpath $classpath ../../src/generateTest/*.java`;
+echo `java -server -Xmx1500m -classpath $classpath generateTest/ChangeFormat $p`;
+
+echo " portion is computed and stored in train.txt and test.txt";
+
+echo `cp train.txt data/geosents600-typed.ccg.dev`;
+echo `cp test.txt  data/geosents280-typed.ccg.test`;
+
+./change.sh data/geosents280-typed.ccg.test
+./change.sh data/geosents600-typed.ccg.dev
+
+echo "train and test has changed";
+
+
+echo `java -server -Xmx1500m -classpath $classpath generateTest/ChangeFormat data/geosents600-typed.ccg.dev`;
+
+echo "format has changed for GIZA++";
+
+
+echo `cp a.txt ../../giza/a.txt`;
+echo `cp b.txt ../../giza/b.txt`;
+
+echo "a.txt and b.txt is copied for GIZA++";
+
+
+
+set a="a.txt";
+set b="b.txt";
+
+set av="a.vcb";
+set bv="b.vcb"
+set ab="a_b.snt";
+
+../../giza/plain2snt.out $a $b
+../../giza/snt2plain.out $av $bv $ab PLAIN
+
+../../giza/mkcls -m2 -pPLAIN1.txt -c50 -V$av.classes opt >& mkcls1.log
+rm PLAIN1.txt
+../../giza/mkcls -m2 -pPLAIN2.txt -c50 -V$bv.classes opt >& mkcls2.log
+rm PLAIN2.txt
+../../giza/snt2cooc.out $av $bv $ab > co.cooc
+../../giza/GIZA++ -S $av -T $bv -C $ab -p0 0.98 -CoocurrenceFile co.cooc  -o fereshte >& fereshte.log
+
+echo `cp fereshte.actual.ti.final data/geo600.dev.giza_probs`;
+
+echo "probs is computed and now we want to run runtest.pl \:D/";
+
+set classpath="../../src:.:/Users/fereshte/Desktop/courses/research/guava-18.0.jar";
+
+echo "javac -classpath $classpath ../../src/*/*.java TestTrain.java";
+echo `javac -classpath $classpath ../../src/*/*.java TestTrain.java`;
+echo "nohup java -server -Xmx1500m -classpath $classpath TestTrain  np-fixedlex.geo >> run.test";
+echo `nohup java -server -Xmx1500m -classpath $classpath TestTrain  np-fixedlex.geo >> run.test`;
+
+
+
+@ p ++;
+end
+
+
